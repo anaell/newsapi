3042a6e6d12d5a7b582e464a0cfb70b32b11e45a
anaell <chimereanaele@gmail.com>
2025-11-20 14:33:06 +0100

    Removed the commented codes at the top of the index.js

--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const port = process.env.PORT;
-
-// const newRoutes = require("./routes/newsRoutes");
-// const podcastRoutes = require("./routes/podcastRoutes");
-
-// require("dotenv").config();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/news", newRoutes);
-// app.use("/api/podcast", podcastRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("Server is up!");
-// });
-
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(() =>
-//     app.listen(port, () => console.log("MongoDB connected, Server running, Server running on http://localhost:5000"))
-//   )
-//   .catch((err) => console.log(`An error happened : ${err}`));
-
-// const express = require("express");
-// const app = express();
-// const port = 5000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello world!");
-// });
-
-// app.listen(port, () =>
-//   console.log(`Test server running on http://localhost:${port}`)
-// );
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -53,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const whitelist = [
- "https://blog-8c82.vercel.app", // production
+  "https://blog-8c82.vercel.app", // production
   "http://127.0.0.1:5500", //local
 ];
 
